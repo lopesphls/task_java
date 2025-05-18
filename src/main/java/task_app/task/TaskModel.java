a6d387abc1be7f71c537b41089f2bbf13bd40f27
Pedro Lopes <lopesphls@gmail.com>
2025-05-18 14:40:14 -0300

    feat: created task model

---
+++ src/main/java/task_app/task/TaskModel.java
@@ -0,0 +1,101 @@
+package task_app.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "task")
+public class TaskModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String name;
+  private Boolean completed;
+  @Column(length = 500)
+  private String description;
+  private LocalDateTime deadline;
+  private Boolean priority;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  public TaskModel() {
+  }
+
+  public TaskModel(String name) {
+    this.name = name;
+  }
+
+  public TaskModel(String name, Boolean completed, String description, LocalDateTime deadline, Boolean priority) {
+    this.name = name;
+    this.completed = completed;
+    this.description = description;
+    this.deadline = deadline;
+    this.priority = priority;
+  }
+
+  public String getId(String id) {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Boolean getCompleted() {
+    return completed;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public LocalDateTime getDeadline() {
+    return deadline;
+  }
+
+  public Boolean getPriority() {
+    return priority;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setCompleted(Boolean completed) {
+    this.completed = completed;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setDeadline(LocalDateTime deadline) {
+    this.deadline = deadline;
+  }
+
+  public void setPriority(Boolean priority) {
+    this.priority = priority;
+  }
+
+}
