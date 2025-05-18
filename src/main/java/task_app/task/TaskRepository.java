8b5832cf53281d144645d13149ef607c791b5ad3
Pedro Lopes <lopesphls@gmail.com>
2025-05-18 14:40:53 -0300

    feat: created task repository

---
+++ src/main/java/task_app/task/TaskRepository.java
@@ -0,0 +1,10 @@
+package task_app.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+}
