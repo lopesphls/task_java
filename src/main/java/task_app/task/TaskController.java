13895e406a6d7286b9039ed97f71f5591802e3f1
Pedro Lopes <lopesphls@gmail.com>
2025-05-18 22:54:43 -0300

    feat: created in controller createTask

--- src/main/java/task_app/task/TaskController.java
+++ src/main/java/task_app/task/TaskController.java
@@ -1,10 +1,17 @@
 package task_app.task;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import task_app.task.Dto.CreateTaskRequest;
 
 @RestController
 @RequestMapping("/task")
@@ -20,4 +27,15 @@ public class TaskController {
   public List<TaskModel> getAll() {
     return service.findAll();
   }
+
+  @GetMapping("/id")
+  public Optional<TaskModel> getOne(@RequestParam UUID id) {
+    return service.findOne(id);
+  }
+
+  @PostMapping()
+  public void createTask(@RequestBody CreateTaskRequest request) {
+    service.create(request.name, request.completed, request.description, request.deadline, request.priority);
+  }
+
 }
