24dd4d71c09e435c3dc620456097a26c368355fa
Pedro Lopes <lopesphls@gmail.com>
2025-05-18 22:40:10 -0300

    feat: create a create dto for request in controller

---
+++ src/main/java/task_app/task/Dto/CreateTaskRequest.java
@@ -0,0 +1,11 @@
+package task_app.task.Dto;
+
+import java.time.LocalDateTime;
+
+public class CreateTaskRequest {
+  public String name;
+  public Boolean completed;
+  public String description;
+  public LocalDateTime deadline;
+  public Boolean priority;
+}
