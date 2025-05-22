3c5ca09d06fca998e6c8d8bc2805418bbc71b871
Pedro Lopes <lopesphls@gmail.com>
2025-05-22 12:36:36 -0300

    feat: created UserModel

---
+++ src/main/java/task_app/user/UserModel.java
@@ -0,0 +1,100 @@
+package task_app.user;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+  @Id
+  private UUID id;
+  @Column(nullable = false)
+  private String name;
+  @Column(nullable = false, unique = true)
+  private String email;
+  @Column(nullable = false)
+  private String password;
+  @Column(nullable = false)
+  private Date birthDate;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  public UserModel() {
+  }
+
+  public UserModel(UUID id, String name, String email, String password, String birtDate) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.birthDate = birthDate;
+  }
+
+  @PrePersist
+  public void prePersist() {
+    if (id == null)
+      id = UUID.randomUUID();
+    this.createdAt = LocalDateTime.now();
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+}
