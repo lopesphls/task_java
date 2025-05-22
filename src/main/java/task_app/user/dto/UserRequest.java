144e8a459b509bcaa457a27d8aba300ca7beef58
Pedro Lopes <lopesphls@gmail.com>
2025-05-22 16:38:26 -0300

    feat: created UserRequest

---
+++ src/main/java/task_app/user/dto/UserRequest.java
@@ -0,0 +1,10 @@
+package task_app.user.dto;
+
+import java.util.Date;
+
+public class UserRequest {
+  public String name;
+  public String email;
+  public String password;
+  public Date birthDate;
+}
