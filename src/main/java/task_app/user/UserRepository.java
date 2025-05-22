fdc1d3df4b4f01355d9c8bfed68c85807fc404c1
Pedro Lopes <lopesphls@gmail.com>
2025-05-22 14:32:35 -0300

    feat: created UserRepository

---
+++ src/main/java/task_app/user/UserRepository.java
@@ -0,0 +1,10 @@
+package task_app.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
