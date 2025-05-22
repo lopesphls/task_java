6d9484ae5cf0008b8b4bc4d63d39952096c63384
Pedro Lopes <lopesphls@gmail.com>
2025-05-22 14:26:10 -0300

    chore: added dotenv and configured in the main

--- src/main/java/task_app/Main.java
+++ src/main/java/task_app/Main.java
@@ -3,11 +3,22 @@ package task_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+  public static void main(String[] args) {
+
+    Dotenv dotenv = Dotenv.load();
+
+    System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+    System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+    System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+    System.setProperty("DB_USER", dotenv.get("DB_USER"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+    SpringApplication.run(Main.class, args);
+  }
 
 }
